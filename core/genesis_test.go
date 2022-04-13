74efbc87a28a7b824c33bfa142546aded4c6eb06
vvvictorlee <lisheng.ipad@icloud.com>
2022-04-13 16:19:17 +0800

    update extdata

--- core/genesis_test.go
+++ core/genesis_test.go
@@ -35,10 +35,10 @@ func TestDefaultGenesisBlock(t *testing.T) {
 	if block.Hash() != params.MainnetGenesisHash {
 		t.Errorf("wrong mainnet genesis hash, got %v, want %v", block.Hash(), params.MainnetGenesisHash)
 	}
-	// block = DefaultRopstenGenesisBlock().ToBlock(nil)
-	// if block.Hash() != params.RopstenGenesisHash {
-	// 	t.Errorf("wrong ropsten genesis hash, got %v, want %v", block.Hash(), params.RopstenGenesisHash)
-	// }
+	block = DefaultTestnetGenesisBlock().ToBlock(nil)
+	if block.Hash() != params.TestnetGenesisHash {
+		t.Errorf("wrong testnet genesis hash, got %v, want %v", block.Hash(), params.TestnetGenesisHash)
+	}
 }
 
 func TestSetupGenesis(t *testing.T) {
